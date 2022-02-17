077d3a8907aee9c2d64ab8db090f7efb0cc35021
Namuna Kassaye <your_email_address@example.com>
2022-02-16 19:47:47 -0800

    update 1

---
+++ service/app.js
@@ -0,0 +1,98 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+//import { Deck } from './models/Deck.js'
+
+const app = express()
+const port = 8000
+
+// Connect to MongoDB
+
+const connectionString = `mongodb+srv://ngk:${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mvxpr.mongodb.net/Note Card?retryWrites=true&w=majority`
+try {
+  await mongoose.connect(connectionString)
+} catch (err) {
+  console.log('error ', err)
+}
+
+// Middleware
+
+/*const exampleMiddleware = (req, res, next) => {
+  console.log('example middleware')
+  next()
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(exampleMiddleware)
+
+// Routes
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+/*app.get('/decks/:id/cards', async (req, res) => {
+  const limit = req.query.limit
+  const deck = await Deck.findById(req.params.id)
+  if (deck) {
+    res.send(deck.cards.slice(0, 5))
+  } else {
+    res.sendStatus(404)
+  }
+})
+
+const cardsById = async (req, res) => {
+  const card = await Deck.findOne({
+    'cards._id': req.params.id
+  })
+  res.status(200).send(card)
+}
+
+app.get('/cards/:id', cardsById)
+
+const isUrl = (value) => {
+  const re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+  return re.test(value)
+}
+
+app.post('/cards', async (req, res) => {
+  const cardRequest = req.body
+  
+  if ((!cardRequest.frontImage && !cardRequest.frontText) || 
+    (!cardRequest.backImage && !cardRequest.backText)) {
+    res.status(400).send('Card data incomplete')
+  }
+
+  if ((frontImage && !isUrl(frontImage)) || (backImage && !isUrl(backImage))) {
+    res.status(400).send('Image fields must be valid URLs')
+  }
+
+  if (!cardRequest.deckId) {
+    res.status(400).send('Deck ID is required')
+  }
+
+  try {
+    const deck = await Deck.findById(cardRequest.deckId)
+    if (deck) {
+      deck.cards.push({
+        frontImage: cardRequest.frontImage,
+        frontText: cardRequest.frontText,
+        backImage: cardRequest.backImage,
+        backText: cardRequest.backText
+      })
+      await deck.save()
+      res.sendStatus(204)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (err) {
+    console.log(`error in creating card ${err}`)
+    res.sendStatus(502)
+  }
+})*/
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
