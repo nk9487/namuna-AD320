8772a2327cb2562c182a9512367f9e7500e5d9dd
Namuna Kassaye <your_email_address@example.com>
2022-02-21 22:27:24 -0800

    get routes added

--- service/app.js
+++ service/app.js
@@ -4,11 +4,11 @@ import cors from 'cors'
 
 import { Deck } from './models/Deck.js'
 
+//start the server
 const app = express()
 const port = 8000
 
 // Connect to MongoDB
-
 const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mvxpr.mongodb.net/FlashCard?retryWrites=true&w=majority`
 try {
   await mongoose.connect(connectionString)
@@ -27,7 +27,7 @@ app.use(cors())
 app.use(express.json())
 app.use(exampleMiddleware)
 
-// Routes
+// Get  Routes
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
@@ -36,17 +36,49 @@ app.get('/', (req, res) => {
 app.get('/decks', (req, res) => {
   res.sendStatus(404)
 })
+//==============================Get Deck by ID ==============================//
 
-app.get('/decks/:id/cards', (req, res) => {
-  const cards = await Deck.findId(req.params.id)
+app.get('/decks/:id/cards', async (req, res) => {
   console.log('request id ', req.params.id)
-  res.sendStatus(503)
+  const limit = req.query.limit
+  console.log('query param limit ', limit)
+  const deck = await Deck.findById(req.params.id)
+  if (deck){
+    console.log('found card', deck.cards.length)
+   res.send(deck.cards.slice(0,3))
+  }else{
+    res.sendStatus(404)
+  }
 })
+//==============================Get Indvidual Card by ID==============================//
+const cardsById = async (req, res) => {
+  const card = await Deck.findOne({
+    'cards._id': req.params.id
+  })
 
-//get by card by ID
-//get all card
-//get deck by ID
-//get deck by user
+  res.status(200).send(card)
+}
+app.get('/cards/:id', cardsById)
+
+//==============================Get A Deck By A User Id==============================//
+
+app.get('/decks/:userId/deckCards', async (req, res) => {
+  console.log('user ID ', req.params.userId)
+  //const limit = req.query.limit
+  //console.log('query param limit ', limit)
+  //const deck = await Deck.findById(req.params.userId)
+  const deck = await Deck.findOne({
+    'cards.userId': req.params.userId
+  })
+  if (deck){
+    console.log('found card', deck.cards.length, deck)
+    console.log(deck.size)
+    console.log(deck.name)
+    res.send(deck.cards)
+  }else{
+    res.sendStatus(404)
+  }
+})
 //create a deck 
 //create card
 //create user
@@ -61,30 +93,15 @@ app.get('/decks/:id/cards', (req, res) => {
 
 
 
-/*app.get('/decks/:id/cards', async (req, res) => {
-  const limit = req.query.limit
-  const deck = await Deck.findById(req.params.id)
-  if (deck) {
-    res.send(deck.cards.slice(0, 5))
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-const cardsById = async (req, res) => {
-  const card = await Deck.findOne({
-    'cards._id': req.params.id
-  })
-  res.status(200).send(card)
-}
-
-app.get('/cards/:id', cardsById)
+//=========================================================================================//
 
 const isUrl = (value) => {
   const re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
   return re.test(value)
 }
 
+
+//==========================================Create Card============================================================================
 app.post('/cards', async (req, res) => {
   const cardRequest = req.body
   
@@ -119,7 +136,7 @@ app.post('/cards', async (req, res) => {
     console.log(`error in creating card ${err}`)
     res.sendStatus(502)
   }
-})*/
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
